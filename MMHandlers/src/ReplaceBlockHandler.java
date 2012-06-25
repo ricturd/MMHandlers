b9bd2c0753257cff4c95f9b9cf511aeaf13ad710
Cybermaxke <cybermaxke@live.be>
2012-06-25 13:58:58 +0200

    Test Replacing

--- MMHandlers/src/ReplaceBlockHandler.java
+++ MMHandlers/src/ReplaceBlockHandler.java
@@ -1,11 +1,14 @@
 import net.morematerials.morematerials.Main;
 import net.morematerials.morematerials.handlers.GenericHandler;
+import net.morematerials.morematerials.materials.SMCustomBlock;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.block.SpoutBlock;
+import org.getspout.spoutapi.material.CustomBlock;
 import org.getspout.spoutapi.player.SpoutPlayer;
-
 
 public class ReplaceBlockHandler extends GenericHandler {
 	
@@ -15,7 +18,14 @@ public class ReplaceBlockHandler extends GenericHandler {
   	public void onActivation(Location loc, SpoutPlayer player) {
   		if (loc == null) return;
   		Block b = loc.getBlock();
-  		b.setType(Material.GLOWSTONE);
+  		if (b instanceof SMCustomBlock) {
+  			//b.breakNaturally();
+  			//b.setType(Material.GLOWSTONE);
+  			SpoutBlock b2 = (SpoutBlock) b;
+  			SpoutManager.getMaterialManager().overrideBlock(b, (CustomBlock) b2);
+  		} else {
+  			b.setType(Material.GLOWSTONE);
+  		}	
   	}
 
   	public void shutdown() {
