d3ef4f3020e96fcd7c291ac24acc52c380f63a14
Cybermaxke <cybermaxke@live.be>
2012-06-25 14:36:35 +0300

    Info added

--- MMHandlers/src/VirtualWorkbenchHandler.java
+++ MMHandlers/src/VirtualWorkbenchHandler.java
@@ -10,10 +10,13 @@ public class VirtualWorkbenchHandler extends GenericHandler {
 	}
 
 	public void onActivation(Location location, SpoutPlayer player) {
+		// If the player is clicking on a door, return.
 		if (location != null) {
 			Material mat = location.getBlock().getType();
-			if (mat.equals(Material.WOODEN_DOOR) || mat.equals(Material.IRON_DOOR) || mat.equals(Material.TRAP_DOOR)) return;
+			if (mat.equals(Material.WOODEN_DOOR) || mat.equals(Material.IRON_DOOR) || mat.equals(Material.TRAP_DOOR) || mat.equals(Material.WOOD_DOOR) || mat.equals(Material.FENCE_GATE) || mat.equals(Material.IRON_DOOR_BLOCK)) return;
 		}
+		
+		// Open the crafting table.
 		player.openWorkbench(null, true);
 	}
 
