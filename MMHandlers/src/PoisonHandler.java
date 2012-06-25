eb9757c41f19ac0c11808c7b7bd489aa85c614cc
Cybermaxke <cybermaxke@live.be>
2012-06-25 14:35:03 +0300

    Info added

--- MMHandlers/src/PoisonHandler.java
+++ MMHandlers/src/PoisonHandler.java
@@ -14,12 +14,15 @@ public class PoisonHandler extends GenericHandler {
   	}
 
   	public void onActivation(Location location, SpoutPlayer player) {
-    		Location loc = player.getLocation();
-   		player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 100, 100));
-   		Particle poisonParticle = new Particle(ParticleType.DRIPWATER, loc, new Vector(0.5D, 3.0D, 0.5D));
-   		poisonParticle.setParticleBlue(0.0F).setParticleGreen(1.0F).setParticleRed(0.0F);
-   		poisonParticle.setMaxAge(40).setAmount(15).setGravity(0.9F);
-   		poisonParticle.spawn();
+  		// Add poison effect to player
+  		player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 100, 100));
+  		
+  		// Playing green drip particles on player location
+  		Location loc = player.getLocation();
+  		Particle poisonParticle = new Particle(ParticleType.DRIPWATER, loc, new Vector(0.5D, 3.0D, 0.5D));
+  		poisonParticle.setParticleBlue(0.0F).setParticleGreen(1.0F).setParticleRed(0.0F);
+  		poisonParticle.setMaxAge(40).setAmount(15).setGravity(0.9F);
+  		poisonParticle.spawn();
   	}
 
   	public void shutdown() {
