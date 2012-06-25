c9e8db5173e24a0752256520bd08f58f3d10fa8d
Cybermaxke <cybermaxke@live.be>
2012-06-25 14:38:22 +0300

    Info added

--- MMHandlers/src/HealingHandler.java
+++ MMHandlers/src/HealingHandler.java
@@ -12,14 +12,17 @@ public class HealingHandler extends GenericHandler {
   	}
 
   	public void onActivation(Location location, SpoutPlayer player) {
-  	Location loc = player.getLocation();
-    	if (player.getHealth() == 20) return;
-    	if (player.getHealth() >= 15) player.setHealth(20); else
-    	player.setHealth(player.getHealth() + 5);
-    	Particle healParticle = new Particle(ParticleType.DRIPWATER, loc, new Vector(0.5D, 3.0D, 0.5D));
-    	healParticle.setParticleBlue(0.0F).setParticleGreen(0.0F).setParticleRed(1.0F);
-    	healParticle.setMaxAge(40).setAmount(15).setGravity(1.1F);
-    	healParticle.spawn();
+  		// Setting player health
+  		if (player.getHealth() == 20) return;
+    		if (player.getHealth() >= 15) player.setHealth(20);
+    		else player.setHealth(player.getHealth() + 5);
+    	
+    		// Playing red drip particles on player location
+  		Location loc = player.getLocation();
+    		Particle healParticle = new Particle(ParticleType.DRIPWATER, loc, new Vector(0.5D, 3.0D, 0.5D));
+    		healParticle.setParticleBlue(0.0F).setParticleGreen(0.0F).setParticleRed(1.0F);
+    		healParticle.setMaxAge(40).setAmount(15).setGravity(1.1F);
+    		healParticle.spawn();
   	}
 
   	public void shutdown() {
