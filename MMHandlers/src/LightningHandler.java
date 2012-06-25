c77ab22a1e7cc39ded0ac3b9ac877613511841b4
Cybermaxke <cybermaxke@live.be>
2012-06-25 14:37:43 +0300

    Info

--- MMHandlers/src/LightningHandler.java
+++ MMHandlers/src/LightningHandler.java
@@ -13,9 +13,14 @@ public class LightningHandler extends GenericHandler {
 	}
 
 	public void onActivation(Location loc, SpoutPlayer player) {
+		// Striking lightning and effect on block location
 		player.getWorld().strikeLightning(loc);
 		player.getWorld().strikeLightningEffect(loc);
+		
+		// Spawn a Pig Zombie
 		player.getWorld().spawnCreature(loc, EntityType.PIG_ZOMBIE);
+		
+		// Playing yellow explosion particles on 1 above block location
 		Location loc2 = loc;
 		loc2.setY(loc2.getY() + 1.0D);
 		Particle smokeParticle = new Particle(ParticleType.EXPLODE, loc2, new Vector(0.5D, 3.0D, 0.5D));
