e08d76081bd3abab9215b26f475034caf94ad57f
tristan <maldouck@hotmail.fr>
2012-08-14 18:28:37 +0200

    updated the handler (not tested yet)

--- MMHandlers/src/FireBallItemHandler.java
+++ MMHandlers/src/FireBallItemHandler.java
@@ -1,24 +1,62 @@
-import net.morematerials.morematerials.Main;
-import net.morematerials.morematerials.handlers.GenericHandler;
-import org.bukkit.Location;
+/**
+ * @version 1.1
+ * @author tristan Roche (Stan_fear)
+ * 
+ * for MoreMaterials 1.8 and later
+ */
+
+import java.util.Map;
+
+import net.morematerials.MoreMaterials;
+import net.morematerials.handlers.GenericHandler;
+
+
 import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
-import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class FireBallItemHandler extends GenericHandler {
-	
-	public void init(Main main) {
-	}
 
-	public void onActivation(Location location, SpoutPlayer player) {
-		Vector direction = player.getEyeLocation().getDirection().multiply(2);
-		Fireball fireball = (Fireball)player.getWorld().spawn(player.getEyeLocation().add(direction.getX(), direction.getY(), direction.getZ()), Fireball.class);
-		fireball.setShooter(player);
-		fireball.setVelocity(direction.multiply(0.25D));
-		fireball.setYield(7.0F);
-		fireball.setIsIncendiary(false);
+	@Override
+	public void init(MoreMaterials plugin) {		
+	}
+	
+	
+	public void onActivation(Event event, Map<String, Object> config) 
+	{
+		// check if the handler (event part) is well configured
+		if(!(event instanceof PlayerInteractEvent))
+			return;
+		
+		// casting the event to have all necessary data
+		PlayerInteractEvent castedEvent = (PlayerInteractEvent) event;
+		
+		// getting the player (to set the origin of the fireball and the direction)
+		Player shooter = castedEvent.getPlayer();
+		
+		// getting the direction the player is looking at
+		Vector playerLookingat = shooter.getEyeLocation().getDirection();
+		
+		// creating the fireball (and launching it ?)
+		Fireball fireball = (Fireball) shooter.getWorld().spawn(
+				shooter.getLocation(), Fireball.class);
+
+		// setting up all never changing parameters
+		fireball.setBounce(false);
+		fireball.setDirection(playerLookingat);
+		fireball.setShooter(shooter);
+		
+		// setting up all parameters set in the config
+		fireball.setYield((float) config.get("power")); // the radius of the explosion
+		fireball.setIsIncendiary((boolean) config.get("setOnFire")); // if the explosion must create fire
+		fireball.setVelocity(new Vector(1,1,1).multiply((float) config.get("velocity")));
+		
+		
 	}
 	
 	public void shutdown() {
 	}
+
 }
